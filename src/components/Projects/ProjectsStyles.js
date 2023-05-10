067ded2576f15bf444d6282da20dd58afea18f6a
Himal Neupane <60805633+nsuzan11@users.noreply.github.com>
2023-05-10 22:09:17 +0545

    edited the card height and width

--- src/components/Projects/ProjectsStyles.js
+++ src/components/Projects/ProjectsStyles.js
@@ -26,7 +26,8 @@ export const BlogCard = styled.div`
   border-radius: 10px;
   box-shadow: 3px 3px 20px rgba(80, 78, 78, 0.5);
   text-align: center;
-  width: 400px;
+  width: 300px;
+  height: 400px;
   @media ${(props) => props.theme.breakpoints.sm} {
     width: 100%;
   }
