b0f47dd745884c9357b298a2a0c9d3388f265d28
Himal Neupane <60805633+nsuzan11@users.noreply.github.com>
2023-05-10 21:53:59 +0545

    Udapted personal accomplishments

--- src/components/Acomplishments/Acomplishments.js
+++ src/components/Acomplishments/Acomplishments.js
@@ -4,7 +4,6 @@ import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalCompon
 import { Box, Boxes, BoxNum, BoxText } from './AcomplishmentsStyles';
 
 const data = [
-  { number: 5, text: 'Open Source Projects'},
   { number: 7, text: 'Certified Qualifications', },
   { number: 1000, text: 'Instagram Followers', }
 ];
